f2aead5a80b6436ecdda93d994638880a256b3ff
Nguyen Thi Thanh Tuyen <camuccon98@gmail.com>
2022-11-04 23:43:38 +0700

    merge 2 files : addNode, deleteNode; create docghifile.h

---
+++ docghifile.h
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include "node.h"
+#include <fstream>
+
+using namespace std;
+
+Node *createNode(string data, int x, int y, int w, int h);
+void createList(listUrl &list);
+void browseNext(listUrl list);
+void removeList(listUrl &list);
+void removeNode(listUrl &list, Node *x);
+bool isEmpty(listUrl list);
+void addTail(listUrl &list, Node*p);
+void docFile(listUrl &list, string fileName, int x, int y, int w, int h, int bg_color, int t_color);
+void ghiFile(listUrl &list, string fileName);
+
+
+void docFile(listUrl &list, string fileName, int x, int y, int w, int h, int bg_color, int t_color) {
+    ifstream infile(fileName);
+    string temp;
+    int i = 0;
+    while (infile >> temp) {
+        Node *p = createNode(temp, 1, 6 + (i * 2), w, h);
+        addTail(list, p);
+        i++;
+    }
+}
+void ghiFile(listUrl &list, string fileName) {
+    ofstream MyFile(fileName);
+    for (Node*i = list.head; i; i = i->next) {
+        MyFile << i->url << endl;
+    }
+    MyFile.close();
+}
